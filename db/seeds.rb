cd46533223a70dbed099068ba414c7b5d1d9c786
Scott Helms <shelms@imageoptions.net>
2018-03-12 01:01:43 -0700

    comment seeds, repair render @customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-Customer.destroy_all
-
-Customer.create!([
-  {
-    id: 1, first_name: 'Bifft', last_name: 'Greiy', email: 'biffteiy@noob.net', 
-    street: '543 Pipedream Ln', city: 'Phargo', zip: '00330'
-  },
-  {
-    id: 2, first_name: 'Syry', last_name: 'Pherggi', email: 'Syryaa@noob.net', 
-    street: '1 Startooter Ave', city: 'Crissdal', zip: 'n/a'
-  },
-  {
-    id: 3, first_name: 'Will', last_name: 'Tan', email: 'joe_blow@noob.net', 
-    street: '2 Smith St', city: 'Bord', zip: '11111'
-  },
-  {
-    id: 4, first_name: 'Ternna', last_name: 'Fliikt', email: 'TurnRndw@noob.net', 
-    street: '50-144 A Spacetube 12', city: 'Flotesviin', zip: '00-W-31'
-  }
-  ])
-  
-  p "Created #{Customer.count} customers."
+# Customer.destroy_all
+# 
+# Customer.create!([
+#   {
+#     id: 1, first_name: 'Bifft', last_name: 'Greiy', email: 'biffteiy@noob.net', 
+#     street: '543 Pipedream Ln', city: 'Phargo', zip: '00330'
+#   },
+#   {
+#     id: 2, first_name: 'Syry', last_name: 'Pherggi', email: 'Syryaa@noob.net', 
+#     street: '1 Startooter Ave', city: 'Crissdal', zip: 'n/a'
+#   },
+#   {
+#     id: 3, first_name: 'Will', last_name: 'Tan', email: 'joe_blow@noob.net', 
+#     street: '2 Smith St', city: 'Bord', zip: '11111'
+#   },
+#   {
+#     id: 4, first_name: 'Ternna', last_name: 'Fliikt', email: 'TurnRndw@noob.net', 
+#     street: '50-144 A Spacetube 12', city: 'Flotesviin', zip: '00-W-31'
+#   }
+#   ])
+# 
+#   p "Created #{Customer.count} customers."
